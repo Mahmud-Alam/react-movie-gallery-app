80136005c6d52375031fb4b1299f2a2ad9092e20
Mahmud-Alam <official.mahmud23@gmail.com>
2025-02-07 00:38:23 +0600

    movie card created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import MovieCard from './components/MovieCard';
+
 function App() {
 
   return (
     <>
+    <MovieCard movie={{title:"Movie Title", release_date:"2024"}}/>
     </>
   )
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,23 @@
-function MovieCard() {
+function MovieCard({ movie }) {
+
+    const onFavoriteClick = () => {
+        alert('clicked!')
+
+    }
+
   return (
-    <div>MovieCard</div>
+    <div className="movie-card">
+        <div className="movie-poster">
+            <img src={movie.url} alt={movie.title} />
+            <div className="movie-overlay">
+                <button className="favorite-btn" onClick={onFavoriteClick}>🤍</button>
+            </div>
+        </div>
+        <div className="movie-info">
+            <h3>{movie.title}</h3>
+            <p>{movie.release_date}</p>
+        </div>
+    </div>
   )
 }
 
