e3afabe616313f6798cf34255ae9187f98c5bccb
mahmud-alam <official.mahmud23@gmail.com>
2025-02-10 10:26:55 +0600

    conditional rendering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,38 @@
-import MovieCard from "../components/MovieCard"
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
-    {id:1, title: "John Wick", release_date: "2010"},
-    {id:2, title: "Mr. Bean", release_date: "2022"},
-    {id:3, title: "The Matrix", release_date: "2026"},
+    { id: 1, title: "John Wick", release_date: "2010" },
+    { id: 2, title: "Mr. Bean", release_date: "2022" },
+    { id: 3, title: "The Matrix", release_date: "2026" },
   ];
 
-  const handleSearch = () => {}
+  const handleSearch = () => {};
 
   return (
     <div className="home">
       <form onSubmit={handleSearch} className="search-form">
-        <input type="text" placeholder="Search for the movies..." className="search-input"/>
-        <button type="submit" className="search-button">Search</button>
+        <input
+          type="text"
+          placeholder="Search for the movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
       </form>
       <div className="movies-grid">
-        {movies.map(movie => <MovieCard movie={movie} key={movie.id}/> )}
+        {movies.map( movie => movie.title.toLowerCase().includes(searchQuery) && (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
