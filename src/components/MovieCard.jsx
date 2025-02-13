666348919f0c7b229a5b9496470247ed436af99b
mahmud-alam <official.mahmud23@gmail.com>
2025-02-13 12:52:07 +0600

    add context to MovieCard and Favorite page: selecting Favorites work now

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,14 @@
 import '../styles/MovieCard.css'
+import { useMovieContext } from '../contexts/MovieContext'
 
 function MovieCard({ movie }) {
+    const {isFavorite, addFavorites, removeFavorites} = useMovieContext()
+    const favorite = isFavorite(movie.id)
 
-    const onFavoriteClick = () => {
-        alert('clicked!')
-
+    const onFavoriteClick = (e) => {
+        e.preventDefault();
+        if(favorite) removeFavorites(movie.id)
+        else addFavorites(movie)
     }
 
   return (
@@ -12,7 +16,7 @@ function MovieCard({ movie }) {
         <div className="movie-poster">
             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
             <div className="movie-overlay">
-                <button className="favorite-btn" onClick={onFavoriteClick}>🤍</button>
+                <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>♥</button>
             </div>
         </div>
         <div className="movie-info">
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,34 @@
-import '../styles/Favorites.css'
+import "../styles/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 function Favorites() {
-  return (
-    <div className="favorites-empty">
-      <h2>No Favorite Movies Yet!</h2>
-      <p>Start adding movies to your favorites and they will appear here.</p>
-    </div>
-  )
+  const { favorites } = useMovieContext();
+
+  if (favorites.length !== 0) {
+    console.log(favorites);
+    
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map(
+            (movie) => (
+              // movie.title.toLowerCase().includes(searchQuery) && (
+              <MovieCard movie={movie} key={movie.id} />
+            )
+            // )
+          )}
+        </div>
+      </div>
+    );
+  } else
+    return (
+      <div className="favorites-empty">
+        <h2>No Favorite Movies Yet!</h2>
+        <p>Start adding movies to your favorites and they will appear here.</p>
+      </div>
+    );
 }
 
-export default Favorites
+export default Favorites;
