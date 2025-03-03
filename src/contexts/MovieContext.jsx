004cc576261f45e27e370211227a9fdd63fd6f26
Mahmud-Alam <official.mahmud23@gmail.com>
2025-03-03 22:43:29 +0600

    [SOLVED] localStorage refreshed empty array issue

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -4,39 +4,61 @@ const MovieContext = createContext();
 
 export const useMovieContext = () => useContext(MovieContext);
 
-export const MovieProvider = ({children}) => {
-    const [favorites, setFavorites] = useState([])
+export const MovieProvider = ({ children }) => {
+  // ERRORS:
+  // 1. When Page is refreshed, favorites are gone from LocalStorage.
+  // 2. using "if (favorites.length > 0)" is solved the issue. but it not removed last favorites, always hold one favorite as lenght > 0.
 
-    useEffect(() => {
-        const storeFavorites = localStorage.getItem("favorites")
+  // SOLUTION:
+  // const [favorites, setFavorites] = useState([]); always set empty array as initial state.
+  // 1. Change the initial state of favorites to a function that returns the parsed value of the stored favorites in localStorage if favorites are available, otherwise a empty array.
+  // 2. Remove the useEffect that sets the initial state of favorites from localStorage.
+  const [favorites, setFavorites] = useState(() => {
+    const storedFavorites = localStorage.getItem("favorites");
+    return storedFavorites ? JSON.parse(storedFavorites) : [];
+  });
 
-        if(storeFavorites) setFavorites(JSON.parse(storeFavorites))
-    },[])
+  /*
+  useEffect(() => {
+    const storeFavorites = localStorage.getItem("favorites");
 
-    useEffect(() => {
-        localStorage.setItem("favorites", JSON.stringify(favorites))
-    },[favorites])
+    if (storeFavorites) setFavorites(JSON.parse(storeFavorites));
+  }, []);
+  */
 
-    const addFavorites = (movie) => {
-        setFavorites(prev => [...prev, movie])
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+
+  /*
+  useEffect(() => {
+//  if (favorites.length > 0) {
+    if (favorites !== undefined) {
+      localStorage.setItem("favorites", JSON.stringify(favorites));
     }
+  }, [favorites]);
+  */
 
-    const removeFavorites = (movieId) => {
-        setFavorites(prev => prev.filter(movie => movie.id !== movieId))
-    }
+  const addFavorites = (movie) => {
+    setFavorites((prev) => [...prev, movie]);
+  };
 
-    const isFavorite = (movieId) => {
-        return favorites.some(movie => movie.id === movieId)
-    }
+  const removeFavorites = (movieId) => {
+    setFavorites((prev) => prev.filter((movie) => movie.id !== movieId));
+  };
 
-    const value = {
-        favorites,
-        addFavorites,
-        removeFavorites,
-        isFavorite
-    }
+  const isFavorite = (movieId) => {
+    return favorites.some((movie) => movie.id === movieId);
+  };
 
-    return <MovieContext.Provider value={value}>
-        {children}
-    </MovieContext.Provider>
-}
+  const value = {
+    favorites,
+    addFavorites,
+    removeFavorites,
+    isFavorite,
+  };
+
+  return (
+    <MovieContext.Provider value={value}>{children}</MovieContext.Provider>
+  );
+};
