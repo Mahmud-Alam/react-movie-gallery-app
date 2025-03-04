2ff7e8954c6e80e4dc90b61f5eccc2d9ef7cd327
mahmud-alam <official.mahmud23@gmail.com>
2025-03-04 14:32:40 +0600

    add logo to Navbar and for mobile device: hamburger icon and rotate animation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import { useState } from "react";
+import logo from "../../public/movie-gallery-app-logo.png"
 
 function NavBar() {
   const [nav, setNav] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const changeValueOnScroll = () => {
     const scrollValue = document?.documentElement?.scrollTop;
@@ -13,17 +15,33 @@ function NavBar() {
 
   addEventListener("scroll", changeValueOnScroll);
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className={`${nav === true ? "sticky navbar" : "navbar"}`}>
-      <div className="navbar-brand">
-        <Link to="/">Movie App</Link>
-      </div>
-      <div className="navbar-links">
+      <Link to="/" className="navbar-brand">
+        <img src={logo} alt="Logo" className="logo" />
+        <p>Movie App</p>
+      </Link>
+      <div className={`navbar-links ${menuOpen ? "open" : ""}`}>
         {/* <Link to='/' className="nav-link">Home</Link> */}
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
         <Link to="/favorites" className="nav-link">
           Favorites
         </Link>
       </div>
+
+      {/* Hamburger icon for mobile */}
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className={`bar ${menuOpen ? "open" : ""}`}></div>
+        <div className={`bar ${menuOpen ? "open" : ""}`}></div>
+        <div className={`bar ${menuOpen ? "open" : ""}`}></div>
+      </div>
+
     </nav>
   );
 }
